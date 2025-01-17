d68cd7c2f9425f913d050e2e6aa9e14dc659e4e0
Vitinho369 <vitinho21.2005@gmail.com>
2025-01-17 18:35:22 -0300

    ajustando reconexao do esp server e envio dos dados pelo ethernet

--- ethernet_blink.c
+++ ethernet_blink.c
@@ -1,86 +1,100 @@
 /*********************
-  Example: Send potentiometer data to Blynk
+  Exemplo: Receber dados do LDR via UART e enviar para o Blynk
  *********************/
 
-/* Fill-in information from Blynk Device Info here */
-#define BLYNK_TEMPLATE_ID           "TMPL2FsxMw0-3"
-#define BLYNK_TEMPLATE_NAME         "Teste"
-#define BLYNK_AUTH_TOKEN            "TUkI8hbMhTZW1-un4wv4peUu7Rjr5ZZY"
+/* Preencha as informações do dispositivo Blynk */
+#define BLYNK_TEMPLATE_ID "TMPL2FsxMw0-3"
+#define BLYNK_TEMPLATE_NAME "Teste"
+#define BLYNK_AUTH_TOKEN "TUkI8hbMhTZW1-un4wv4peUu7Rjr5ZZY"
+
 #include <SoftwareSerial.h>
-
-// Configura SoftwareSerial nos pinos 10 e 11
-SoftwareSerial mySerial(10, 11); // RX = pino 10, TX = pino 11
-
-/* Comment this out to disable prints and save space */
-#define BLYNK_PRINT Serial
-
-char auth[] = "TUkI8hbMhTZW1-un4wv4peUu7Rjr5ZZY";
 #include <SPI.h>
 #include <Ethernet.h>
 #include <BlynkSimpleEthernet.h>
 
-// Endereço MAC (verifique a etiqueta do Ethernet Shield ou use um endereço aleatório)
+// Configura SoftwareSerial nos pinos 10 e 11
+SoftwareSerial mySerial(10, 11); // RX = pino 10, TX = pino 11
+String receivedMessage = "";  // Variable to store the complete message
+
+/* Define para habilitar prints para depuração */
+#define BLYNK_PRINT Serial
+
+bool send_blynk = false;
+volatile bool *send_blynk_pointer = &send_blynk;
+unsigned long time;
+
+// Credenciais Blynk
+char auth[] = BLYNK_AUTH_TOKEN;
+
+// Endereço MAC (use um endereço fixo ou aleatório para o Ethernet Shield)
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
 
-// IP fixo (opcional, pode usar DHCP)
-IPAddress ip(10,77,12,103);  // Ou deixe o DHCP configurar automaticamente
+// Endereço IP fixo (opcional, pode usar DHCP)
+IPAddress ip(10, 77, 12, 103); // Ajuste conforme sua rede
 
+// Inicializa o temporizador do Blynk
 BlynkTimer timer;
 
-// Pino analógico onde o potenciômetro está conectado
-int sensorValue = 0;  // Conecte o potenciômetro ao pino A0 ou outro pino analógico
+// Variável para armazenar o valor do LDR recebido via UART
+int ldrValue = 0;
 
-// Função para enviar os dados do potenciômetro para o Blynk
-void sendPotentiometerData()
-{
-  // Lê o valor do potenciômetro (0 a 1023)
- // int sensorValue = map(analogRead(potPin),0,1023,0,255);
+// Função para enviar os dados do LDR para o Blynk
+void sendLdrDataToBlynk() {
+  // Envia o valor do LDR para o pino virtual V1 no Blynk
+  if(*send_blynk_pointer){
+    Blynk.virtualWrite(V1, ldrValue);
 
-  //Serial.println(sensorValue);
-  
-  // Envia o valor para o pino virtual V1 no Blynk (pode ser ajustado conforme necessidade)
-  Blynk.virtualWrite(V1,sensorValue);  // Mapeia o valor de 0-1023 para 0-255, se necessário
-
-  Blynk.virtualWrite(V2,sensorValue);  // Mapeia o valor de 0-1023 para 0-255, se necessário
-
+    // Debug no monitor serial
+    Serial.print("Valor do LDR enviado para o Blynk: ");
+    Serial.println(ldrValue);
+  }
 }
 
 // Função chamada toda vez que o dispositivo se conecta ao Blynk.Cloud
-BLYNK_CONNECTED()
-{
+BLYNK_CONNECTED() {
+  // Configura propriedades do botão no Blynk
   Blynk.setProperty(V3, "offImageUrl", "https://static-image.nyc3.cdn.digitaloceanspaces.com/general/fte/congratulations.png");
-  Blynk.setProperty(V3, "onImageUrl",  "https://static-image.nyc3.cdn.digitaloceanspaces.com/general/fte/congratulations_pressed.png");
+  Blynk.setProperty(V3, "onImageUrl", "https://static-image.nyc3.cdn.digitaloceanspaces.com/general/fte/congratulations_pressed.png");
   Blynk.setProperty(V3, "url", "https://docs.blynk.io/en/getting-started/what-do-i-need-to-blynk/how-quickstart-device-was-made");
 }
 
 // Função de temporizador para enviar dados a cada 1 segundo
-void myTimerEvent()
-{
-  sendPotentiometerData();  // Chama a função para enviar os dados do potenciômetro
+void myTimerEvent() {
+  sendLdrDataToBlynk(); // Envia os dados do LDR
 }
 
-void setup()
-{
-  // Inicializa a porta serial para debug
-  Serial.begin(115200);
+void setup() {
+  // Inicializa a porta serial para depuração
+  Serial.begin(9600);
 
+  // Inicializa o SoftwareSerial
   mySerial.begin(9600);
-  // Inicializa a Ethernet (DHCP ou IP fixo)
-   // Para DHCP: Ethernet.begin(mac);
-  
-  // Inicia o Blynk
-  Blynk.begin(auth);
- // pinMode(potPin, INPUT);
-  // Configura o temporizador para enviar dados do potenciômetro a cada 1 segundo
+
+  // Inicializa o Blynk
+  // Blynk.begin(auth, mac, ip);
+
+  // Executa o Blynk
+  Serial.println("Blynk iniciado");
+  // Configura o temporizador para enviar dados do LDR a cada 1 segundo
   timer.setInterval(1000L, myTimerEvent);
+
+  Serial.println("Arduino iniciado e aguardando dados do ESP32 via UART.");
+  time = millis();
 }
 
-void loop()
-{
+void loop() {
+  // Executa o Blynk
   Blynk.run();
+
+  // Executa o temporizador
   timer.run();
-  if (mySerial.available()) {
+  // Verifica se há dados recebidos via UART
+   if (mySerial.available()) {
     String receivedData = mySerial.readStringUntil('\n'); // Lê os dados até '\n'
-    Serial.println(receivedData);
+    ldrValue = receivedData.toInt();
+    send_blynk = true;
+  }else if (millis() - time > 1000){
+    send_blynk = false;
+    time = millis();
   }
 }
--- serve.c
+++ serve.c
@@ -7,14 +7,18 @@
 #include <BlynkSimpleEsp32.h>
 
 char auth[] = BLYNK_AUTH_TOKEN;
-char ssid[] = "A";        
-char pass[] = "12345678";  
+char ssid[] = "A";
+char pass[] = "12345678";
 
+const char *ssid_esp = "ESP32_AP"; // Nome da rede criada pelo ESP32 (AP)
+const char *pass_esp = "12345678"; // Senha da rede criada pelo ESP32 (AP)
+
+bool blynk_connect = false;
 BlynkTimer timer;
 
-#define LDR_PIN 34 
+#define LDR_PIN 34
 
-HardwareSerial SerialPort(2); 
+HardwareSerial SerialPort(2);
 int cont = 0;
 void sendSensor()
 {
@@ -23,32 +27,70 @@ void sendSensor()
   int ldrValue = analogRead(LDR_PIN);
   ldrValue = cont;
   // Verifica se está conectado ao Wi-Fi e ao Blynk
-  if (WiFi.status() == WL_CONNECTED && Blynk.connected()) {
+  if (WiFi.status() == WL_CONNECTED && Blynk.connected())
+  {
     // Envia o valor para o Blynk (Virtual Pin V1)
     Blynk.virtualWrite(V1, ldrValue);
 
     // Mensagem para depuração
     Serial.print("LDR Value sent to Blynk: ");
     Serial.println(ldrValue);
-  } else {
+  }
+  else
+  {
     // Envia os dados via UART para o Arduino
     Serial.print("Sending to Arduino via UART: ");
     Serial.println(ldrValue);
-    SerialPort.println(ldrValue);  // Envia o valor pela UART
+    SerialPort.println(ldrValue); // Envia o valor pela UART
   }
+}
+
+bool connect_network()
+{
+  Serial.println("Scanneando redes");
+  int n = WiFi.scanNetworks();
+  if (n > 0)
+  {
+    for (int i = 0; i < n; ++i)
+    {
+      if (WiFi.SSID(i) == "A")
+        return true;
+    }
+  }
+  return false;
 }
 
 void setup()
 {
-  Serial.begin(115200);  // Inicializa o monitor serial
+  Serial.begin(115200); // Inicializa o monitor serial
   Serial.println("Starting ESP32...");
 
-  SerialPort.begin(9600, SERIAL_8N1, 16, 17); 
+  SerialPort.begin(9600, SERIAL_8N1, 16, 17);
   Serial.println("UART2 inicializada");
-
   // Conecta ao Blynk e Wi-Fi
   Serial.println("Connecting to Wi-Fi...");
-  Blynk.begin(auth, ssid, pass);
+
+  WiFi.mode(WIFI_AP_STA);
+
+  bool apStarted = WiFi.softAP(ssid_esp, pass_esp);
+  if (apStarted)
+  {
+    Serial.println("Access Point criado com sucesso!");
+    Serial.print("Nome da rede (AP): ");
+    Serial.println(ssid_esp);
+    Serial.print("Endereço IP (AP): ");
+    Serial.println(WiFi.softAPIP());
+  }
+  else
+  {
+    Serial.println("Falha ao criar o Access Point.");
+  }
+
+  if (connect_network())
+  {
+    blynk_connect = true;
+    Blynk.begin(auth, ssid, pass);
+  }
 
   // Configura o timer para enviar os dados a cada 1 segundo (1000ms)
   timer.setInterval(1000L, sendSensor);
@@ -58,17 +100,27 @@ void setup()
   Serial.println("\nConectado à rede Wi-Fi!");
   Serial.print("Endereço IP: ");
   Serial.println(WiFi.localIP());
-  SerialPort.println("Conectado à rede Wi-Fi!"); 
-
 }
 
 void loop()
 {
   // Executa as funções do Blynk se estiver conectado
-  if (WiFi.status() == WL_CONNECTED) {
+  if (blynk_connect && WiFi.status() == WL_CONNECTED)
+  {
+    Serial.println("blynk run");
     Blynk.run();
+  }
+  else
+  {
+    blynk_connect = false;
   }
 
   // Executa o timer independentemente da conexão
   timer.run();
+
+  if (!blynk_connect && connect_network())
+  {
+    blynk_connect = true;
+    Blynk.begin(auth, ssid, pass);
+  }
 }
