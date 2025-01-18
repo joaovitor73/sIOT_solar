7f3583d75e9b15729c75c49f7ff528d4b72a46d8
Vitinho369 <vitinho21.2005@gmail.com>
2025-01-17 21:33:55 -0300

    Reconectando-se a rede

--- serve.c
+++ serve.c
@@ -15,6 +15,8 @@ const char *pass_esp = "12345678"; // Senha da rede criada pelo ESP32 (AP)
 
 bool blynk_connect = false;
 BlynkTimer timer;
+unsigned long temp;
+unsigned long temp_reconnect = 0;
 
 #define LDR_PIN 34
 
@@ -45,21 +47,6 @@ void sendSensor()
   }
 }
 
-bool connect_network()
-{
-  Serial.println("Scanneando redes");
-  int n = WiFi.scanNetworks();
-  if (n > 0)
-  {
-    for (int i = 0; i < n; ++i)
-    {
-      if (WiFi.SSID(i) == "A")
-        return true;
-    }
-  }
-  return false;
-}
-
 void setup()
 {
   Serial.begin(115200); // Inicializa o monitor serial
@@ -71,6 +58,9 @@ void setup()
   Serial.println("Connecting to Wi-Fi...");
 
   WiFi.mode(WIFI_AP_STA);
+  WiFi.persistent(false);      // Impede que o ESP32 salve credenciais na memória flash
+  WiFi.setAutoReconnect(true); // Habilita a reconexão automática
+  WiFi.begin(ssid, pass);      // Inicia a conexão Wi-Fi
 
   bool apStarted = WiFi.softAP(ssid_esp, pass_esp);
   if (apStarted)
@@ -86,7 +76,7 @@ void setup()
     Serial.println("Falha ao criar o Access Point.");
   }
 
-  if (connect_network())
+  if (WiFi.status() == WL_CONNECTED)
   {
     blynk_connect = true;
     Blynk.begin(auth, ssid, pass);
@@ -100,6 +90,8 @@ void setup()
   Serial.println("\nConectado à rede Wi-Fi!");
   Serial.print("Endereço IP: ");
   Serial.println(WiFi.localIP());
+  temp = millis();
+  temp_reconnect = millis();
 }
 
 void loop()
@@ -107,7 +99,6 @@ void loop()
   // Executa as funções do Blynk se estiver conectado
   if (blynk_connect && WiFi.status() == WL_CONNECTED)
   {
-    Serial.println("blynk run");
     Blynk.run();
   }
   else
@@ -118,9 +109,21 @@ void loop()
   // Executa o timer independentemente da conexão
   timer.run();
 
-  if (!blynk_connect && connect_network())
+  if (!blynk_connect && WiFi.status() == WL_CONNECTED)
   {
     blynk_connect = true;
     Blynk.begin(auth, ssid, pass);
+  }
+
+  temp = millis();
+  if ((temp - temp_reconnect >= 30000))
+  {
+    if ((WiFi.status() != WL_CONNECTED))
+    {
+      Serial.println("Reconnecting to WiFi...");
+      WiFi.disconnect();
+      WiFi.reconnect();
+    }
+    temp_reconnect = temp;
   }
 }
