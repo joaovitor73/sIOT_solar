82587b4c528b1c0e10bf977d0a2eb70cc524dcaa
Vitinho369 <vitinho21.2005@gmail.com>
2025-01-22 11:28:55 -0300

    colocando threads na interface grafica

--- monitor_serial.py
+++ monitor_serial.py
@@ -1,34 +1,43 @@
 import serial
-import numpy
 import matplotlib.pyplot as plt
 from drawnow import *
+from threading import Thread, Lock
+
 luminosidade = []
 conexao = serial.Serial('com10', 115200)
 plt.ion()
 cont = 0
-def makeFig(): 
-    plt.ylim(0,255) 
+
+data_lock = Lock()
+
+def makeFig():
+    plt.ylim(0, 255)
     plt.title('Sensores')
     plt.grid(True)
     plt.ylabel('Luminosidade')
-    plt.plot(luminosidade,'ro-', label='Luminosidade')
-       
+    plt.plot(luminosidade, 'ro-', label='Luminosidade')
+
+def read_from_arduino():
+    global cont
+    while True:
+        try:
+            if conexao.in_waiting > 0:
+                arduinoString = conexao.readline().decode('utf-8').strip()
+                dataArray = arduinoString.split(' , ')
+                print(dataArray)
+                if 'connecting' not in arduinoString:
+                    ldr = float(dataArray[0])
+                    
+                    with data_lock:
+                        luminosidade.append(ldr)
+                        if len(luminosidade) > 50:
+                            luminosidade.pop(0)
+        except Exception as e:
+            print(f"Erro na leitura: {e}")
+
+thread = Thread(target=read_from_arduino, daemon=True)
+thread.start()
+
 while True:
-    while (conexao.inWaiting()==0):
-        pass 
-
-    try:
-        arduinoString = conexao.readline().decode('utf-8')
-
-        dataArray = arduinoString.split(' , ')
-
-        ldr = float(dataArray[0]) 
-        luminosidade.append(ldr)
+    with data_lock:
         drawnow(makeFig)
-        cont = cont +1
-
-        if(cont > 50):
-            luminosidade.pop(0)
-    except:
-        pass
-
